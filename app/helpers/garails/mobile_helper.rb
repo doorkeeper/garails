704635d141f317cce398321a4241ddff4ee8c1a3
Paul McMahon <paul@mobalean.com>
2011-04-19 10:50:50 +0900

    Use path instead of url to handle ssl pages properly

--- app/helpers/garails/mobile_helper.rb
+++ app/helpers/garails/mobile_helper.rb
@@ -1,10 +1,10 @@
 module Garails::MobileHelper
 
   def utm_tag
-    image_tag utm_url, :alt => "", :size => "1x1"
+    image_tag utm_path, :alt => "", :size => "1x1"
   end
 
-  def utm_url
+  def utm_path
     referer = request.env['HTTP_REFERER']
     params = {
       :utmn => rand(0x7fffffff).to_s,
