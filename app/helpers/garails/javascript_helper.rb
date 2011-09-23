e895a0bdaa3780c8d6071940b46521136b027735
Paul McMahon <paul@mobalean.com>
2011-09-23 11:00:03 +0900

    Add option for extra GA account

--- app/helpers/garails/javascript_helper.rb
+++ app/helpers/garails/javascript_helper.rb
@@ -1,7 +1,7 @@
 module Garails::JavascriptHelper
-  def google_analytics_tracking_javascript
+  def google_analytics_tracking_javascript(extra_account=nil)
     if Garails.ga_setup?
-      render :partial => "garails/google_analytics"
+      render :partial => "garails/google_analytics", :locals => { :extra_account => extra_account }
     else
       render :partial => "garails/no_google_analytics"
     end
